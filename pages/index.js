6cca20b9352e43f4db60d1d68b6e8e68575619fb
Rafael Fernandes <rafaellemosfernandes@gmail.com>
2022-03-01 15:45:27 -0300

    bug consertado httpEquiv

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home({ artigos }) {
       <Head>
         <title>Mario Filho & Advogados | Escritório de Advocacia</title>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
